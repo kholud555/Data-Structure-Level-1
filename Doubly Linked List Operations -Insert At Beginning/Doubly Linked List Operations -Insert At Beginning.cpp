36016b0e21ff96262b0791457f43d00daa44cc13
KHOLUD AHMED <kokhaahmed315@gmail.com>
2024-11-06 01:20:16 +0200

    second commit

--- Doubly Linked List Operations -Insert At Beginning/Doubly Linked List Operations -Insert At Beginning.cpp
+++ Doubly Linked List Operations -Insert At Beginning/Doubly Linked List Operations -Insert At Beginning.cpp
@@ -1,20 +1,91 @@
-// Doubly Linked List Operations -Insert At Beginning.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
 
-int main()
+class Node {
+
+public:
+    int Value;
+    Node* next;
+    Node* prev;
+
+};
+void InsertAtBegining(Node*& head, int Value)
 {
-    std::cout << "Hello World!\n";
+    Node* new_node = new Node();
+
+    new_node->Value = Value;
+    new_node->next = head;
+    new_node->prev = NULL;
+
+    if (head != NULL)
+    {
+        head->prev = new_node;
+    }
+    
+    head = new_node;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void PrintNodeDetails(Node* head)
+{
+
+    if (head->prev != NULL)
+        cout << head->prev->Value;
+    else
+        cout << "NULL";
+
+    cout << " <--> " << head->Value << " <--> ";
+
+    if (head->next != NULL)
+        cout << head->next->Value << "\n";
+    else
+        cout << "NULL";
+
+}
+
+// Print the linked list
+void PrintListDetails(Node* head)
+
+{
+    cout << "\n\n";
+    while (head != NULL) {
+        PrintNodeDetails(head);
+        head = head->next;
+    }
+}
+
+
+// Print the linked list
+void PrintList(Node* head)
+
+{
+    cout << "NULL <--> ";
+    while (head != NULL) {
+        cout << head->Value << " <--> ";
+        head = head->next;
+    }
+    cout << "NULL";
+
+}
+
+
+
+int main()
+{ 
+    Node* head = NULL;
+
+    InsertAtBegining(head, 5);
+    InsertAtBegining(head, 4);
+    InsertAtBegining(head, 3);
+    InsertAtBegining(head, 2);
+    InsertAtBegining(head, 1);
+
+    cout << "\nLinked List Contenet:\n";
+    PrintList(head);
+    PrintListDetails(head);
+    system("pause>0");
+    
+}
+
